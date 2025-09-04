01db7b210654107567954aec4959f582f8ab0b94
Anouar <anouar.laachir2024@gmail.com>
2025-09-04 21:04:24 +0100

    Adding a basic MainController to test the API

---
+++ src/main/java/com/fullstack/jwt_spring/MainController.java
@@ -0,0 +1,14 @@
+package com.fullstack.jwt_spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MainController {
+    @GetMapping("/index")
+    public String index() {
+        return "Hello World";
+    }
+}
