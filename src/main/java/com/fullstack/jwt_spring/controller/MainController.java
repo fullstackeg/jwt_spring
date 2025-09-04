681b3ba8382b64f2fe60b02a701d151cfdf9eae0
Anouar <anouar.laachir2024@gmail.com>
2025-09-04 22:18:24 +0100

    Adding User entity & repository

--- src/main/java/com/fullstack/jwt_spring/controller/MainController.java
+++ src/main/java/com/fullstack/jwt_spring/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.fullstack.jwt_spring;
+package com.fullstack.jwt_spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
